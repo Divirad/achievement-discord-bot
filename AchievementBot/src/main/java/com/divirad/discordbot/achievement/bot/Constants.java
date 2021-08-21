8edb2c71c4349ee4a3613334a18f121dbc5c7d7f
Nicolas Kepper <nicolas.kepper@gmail.com>
2021-08-21 12:31:32 +0200

    Get token from env instead of hardcoded

--- AchievementBot/src/main/java/com/divirad/discordbot/achievement/bot/Constants.java
+++ AchievementBot/src/main/java/com/divirad/discordbot/achievement/bot/Constants.java
@@ -2,5 +2,5 @@ package com.divirad.discordbot.achievement.bot;
 
 public class Constants {
 
-	public static final String BOT_TOKEN = "ODQwNTcxNjM0ODk2NzMyMjIw.YJaJVg._Kgh7A6-VrCvZ_95txcjYxhLaIw";
+	public static final String BOT_TOKEN = System.getenv("BOT_TOKEN");
 }
